2de7aa3c351587e2f3fc72d33c073b94c43ff681
AuggieH <hudakjerome@gmail.com>
2020-04-11 15:40:11 -0400

    fix module 3 assertion messages

--- test/module-3/App.test.js
+++ test/module-3/App.test.js
@@ -20,20 +20,34 @@ describe('App', () => {
     const instance = wrapper.instance()
     instance.setState({tiles: [{id: 1, color: '#'}]})
     const tiles = instance.state.tiles
-    instance.handleTileClicked(tiles[0].id, tiles[0].color)
 
-    expect(setStateSpy.mock.calls[1][0].toBeCleared, 'Did you call setState with the correct values?').toBe(null)
-    expect(setStateSpy.mock.calls[1][0].tiles, 'Did you call setState with the correct values?').toEqual(expect.any(Array))
+    let mockCall
+    let cleared
+    let mockTiles
+
+    try {
+      instance.handleTileClicked(tiles[0].id, tiles[0].color)
+      mockCall = setStateSpy.mock.calls[1][0]
+      cleared = mockCall.toBeCleared
+      mockTiles = mockCall.tiles
+    } catch(error) {}
+
+    expect(cleared, 'Did you call setState with the correct values?').toBe(null)
+    expect(mockTiles, 'Did you call setState with the correct values?').toEqual(expect.any(Array))
   })
 
   it('sets the selected tile as the previous tile if its null @find-the-selected-tile', () => {
     const setStateSpy = jest.spyOn(App.prototype, 'setState')
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
+
+    let tiles
+    try {
+      instance.startGame(10)
+      tiles = instance.state.tiles
+      instance.handleTileClicked(tiles[5].id, tiles[5].color)
+    } catch(error) {}
     
-    const tiles = instance.state.tiles
-    instance.handleTileClicked(tiles[5].id, tiles[5].color)
 
     expect(setStateSpy, 'Did you set the previousTileIndex to the selectedTileIndex?')
       .toHaveBeenCalledWith({ previousTileIndex: 5, tiles, toBeCleared: null})
@@ -43,42 +57,50 @@ describe('App', () => {
   it('handles matched tiles @handle-matched-tile', () => {
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
 
-    let tiles = instance.state.tiles
+    let tiles
+    let selectedTile
+    let matchingPreviousTileIndex
+    let fifthTileMatched
+    let matching
 
-    const selectedTile = tiles[5]
+    try {
+      instance.startGame(10)
+      tiles = instance.state.tiles
+      selectedTile = tiles[5]
 
-    const matchingPreviousTileIndex = instance.state.tiles.findIndex((tile) => {
-      return tile.color === selectedTile.color && tile.key !== selectedTile.key
-    })
+      matchingPreviousTileIndex = instance.state.tiles.findIndex((tile) => {
+        return tile.color === selectedTile.color && tile.key !== selectedTile.key
+      })
 
-    instance.setState({previousTileIndex: matchingPreviousTileIndex})
+      instance.setState({previousTileIndex: matchingPreviousTileIndex})
+      instance.handleTileClicked(selectedTile.id, selectedTile.color)
+      tiles = instance.state.tiles
+      fifthTileMatched = tiles[5].matched
+      matching = tiles[matchingPreviousTileIndex].matched
+    } catch (error) {}
 
-    instance.handleTileClicked(selectedTile.id, selectedTile.color)
-
-
-    tiles = instance.state.tiles
-    expect(tiles[5].matched, 'Did you set the matched property of the selected tile to true?').toBe(true)
-    expect(tiles[matchingPreviousTileIndex].matched, 'Did you set the matched property of the previous tile to true?').toBe(true)
+    expect(fifthTileMatched, 'Did you set the matched property of the selected tile to true?').toBe(true)
+    expect(matching, 'Did you set the matched property of the previous tile to true?').toBe(true)
     expect(instance.state.previousTileIndex).toBe(null)
   })
 
   it('handles mismatched tiles @handle-mismatched-tile', () => {
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
+    let tiles
+    let toBeCleared
 
-    let tiles = instance.state.tiles
+    try {
+      instance.startGame(10)
+      tiles = instance.state.tiles
+      instance.setState({previousTileIndex: 0})
+      instance.handleTileClicked(tiles[5].id, tiles[5].color)
+      tiles = instance.state.tiles
+      toBeCleared = instance.state.toBeCleared
+    } catch (eror) {}
 
-
-    instance.setState({previousTileIndex: 0})
-
-    instance.handleTileClicked(tiles[5].id, tiles[5].color)
-
-
-    tiles = instance.state.tiles
-    expect(instance.state.toBeCleared, 'Did you add the previous and selected tiles to toBeCleared?')
+    expect(toBeCleared, 'Did you add the previous and selected tiles to toBeCleared?')
       .toEqual([0,5])
     expect(instance.state.previousTileIndex, 'Did you set the previousTileIndex to nulll?').toBe(null)
 
@@ -87,45 +109,68 @@ describe('App', () => {
   it('clears mismatched tiles @clear-mismatched-tiles', () => {
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
+    let tiles
+    let toBeCleared
+    let zeroethSelected
+    let firstSelected
 
-    instance.setState((state) => {
-      const tiles = state.tiles
-      tiles[0].selected = true
-      tiles[1].selected = true
+    try {
+      instance.startGame(10)
 
-      const toBeCleared = [0, 1]
-      return { tiles, toBeCleared, previousTileIndex: null }
-    })
+      instance.setState((state) => {
+        const tiles = state.tiles
+        tiles[0].selected = true
+        tiles[1].selected = true
 
-    const tiles = instance.state.tiles
-    instance.handleTileClicked(tiles[3].id, tiles[3].color)
+        const toBeCleared = [0, 1]
+        return { tiles, toBeCleared, previousTileIndex: null }
+      })
 
-    expect(instance.state.toBeCleared, 'Did you set toBeCleared to null?').toBe(null)
-    expect(tiles[0].selected, 'Did you remember to set the first tile in toBeSelected selected property to false?')
+      tiles = instance.state.tiles
+      instance.handleTileClicked(tiles[3].id, tiles[3].color)
+
+      toBeCleared = instance.state.toBeCleared
+      zeroethSelected = tiles[0].selected
+      firstSelected = tiles[1].selected
+    } catch(error) {}
+
+    expect(toBeCleared, 'Did you set toBeCleared to null?').toBe(null)
+    expect(zeroethSelected, 'Did you remember to set the first tile in toBeSelected selected property to false?')
       .toBe(false)
-    expect(tiles[1].selected, 'Did you remember to set the second tile in toBeSelected selected property to false?')
+    expect(firstSelected, 'Did you remember to set the second tile in toBeSelected selected property to false?')
       .toBe(false)
   })
 
   it('sets the clicked tile to selected @selected-tile', () => {
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
+    let tiles
+    let thirdSelected
 
-    const tiles = instance.state.tiles
-    instance.handleTileClicked(tiles[3].id, tiles[3].color)
+    try {
+      instance.startGame(10)
 
-    expect(tiles[3].selected, 'Did you set the selected property on the clicked tile?').toBe(true)
+      tiles = instance.state.tiles
+      instance.handleTileClicked(tiles[3].id, tiles[3].color)
+      thirdSelected = tiles[3].selected
+    } catch(error) {}
+
+    expect(thirdSelected, 'Did you set the selected property on the clicked tile?').toBe(true)
   })
     
   it('attaches the handleTileClicked method to the tiles in the array @handle-tile-clicked-array', () => {
     const wrapper = shallow(<App />)
     const instance = wrapper.instance()
-    instance.startGame(10)
+    let tiles
+    let thirdHandler
 
-    const tiles = instance.state.tiles
+    try {
+      instance.startGame(10)
 
-    expect(typeof tiles[3].handleTileClicked, 'Did you add the handleTileClicked method to the call to createTiles?').toBe('function')
+      tiles = instance.state.tiles
+      thirdHandler = tiles[3].handleTileClicked
+    } catch(error) {}
+
+    expect(typeof thirdHandler, 'Did you add the handleTileClicked method to the call to createTiles?').toBe('function')
   })
 })
