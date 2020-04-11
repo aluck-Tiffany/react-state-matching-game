190c9d0375d2a1360662a9b5f2bbccbae76d0d6a
AuggieH <hudakjerome@gmail.com>
2020-04-11 18:14:47 -0400

    revert tests to previous state

--- test/module-5/refactor.test.js
+++ test/module-5/refactor.test.js
@@ -2,27 +2,11 @@ import React from 'react'
 
 describe('Refactor', () => {
   it('copies the src directory to a new directory called refactor @copy-app', () => {
-    let App
-    let Board
-    let Button
-    let OptionsPanel
-    let Tile
-    let TileSelector
-
-    try {
-      App = require('../../refactor/components/App/App.js')
-      Board = require('../../refactor/components/Board/Board.js')
-      Button = require('../../refactor/components/Button/Button.js')
-      OptionsPanel = require('../../refactor/components/OptionsPanel/OptionsPanel.js')
-      Tile = require('../../refactor/components/Tile/Tile.js')
-      TileSelector = require('../../refactor/components/TileSelector/TileSelector.js')
-    } catch(error) {}
-
-    expect(App, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
-    expect(Board, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
-    expect(Button, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
-    expect(OptionsPanel, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
-    expect(Tile, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
-    expect(TileSelector, 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/App/App.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/Board/Board.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/Button/Button.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/OptionsPanel/OptionsPanel.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/Tile/Tile.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
+    expect(require('../../refactor/components/TileSelector/TileSelector.js'), 'Did you copy over the src directory into a new directory refactor?').toBeTruthy()
   })
 })
